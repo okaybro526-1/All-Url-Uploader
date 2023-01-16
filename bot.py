ae1f5d22a96dcbf1d8a7aaf028aa417e4ac4b87d
Xtron [@pro_noober] <84863955+okaybro526-1@users.noreply.github.com>
2023-01-16 16:23:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,39 @@
-# MIT License
-
-# Copyright (c) 2022 Hash Minner
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE
-
 import os
 from pyrogram import Client, idle
-
-if bool(os.environ.get("WEBHOOK")):
-    from Uploader.config import Config
-else:
-    from sample_config import Config
-
-import os
-
+from Uploader.config import Config
 import logging
+from pyrogram.raw import functions, types
+from pyromod import listen
+
+
+STATUS=Config.STATUS
+
+USER=Config.USER
+bot = Client(
+    "InstaSessibon",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    workers=50,
+    plugins=dict(root="plugins")
+    )
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+async def main():
+    async with bot:
+        await bot.download_media(Config.INSTA_SESSIONFILE_ID, file_name=f"./{Config.USER}")
+        Config.L.load_session_from_file(USER, filename=f"./{USER}")
+        STATUS.add(1)
+
+if Config.INSTA_SESSIONFILE_ID:
+    bot.run(main())
+    
+bot.start()
 
 if __name__ == "__main__":
 
